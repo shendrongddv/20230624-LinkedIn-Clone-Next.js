d5b0c6ffd9fe155f050cb42a75326e7b4d15986c
shendrongddv <shendrongddv@gmail.com>
2023-06-24 16:44:58 +0700

    Sabtu 24 Juni 2023

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,13 @@
-import React from "react";
-
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className="px-6">
+      <div className="container flex h-screen items-center justify-center">
+        <h1 className="text-center text-3xl font-medium text-black/75 md:text-6xl">
+          Selamat datang di komunitas profesional Anda
+        </h1>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
