a3580a22e7ff1cbb6bc584d96351350a22d982f5
shendrongddv <shendrongddv@gmail.com>
2023-06-24 15:16:59 +0700

    Sabtu 24 Juni 2023

---
+++ dummy/index.ts
@@ -0,0 +1,231 @@
+// footerMenu
+export const footerMenu = [
+  {
+    id: 1,
+    heading: "Umum",
+    subMenu: [
+      {
+        id: 1,
+        label: "Daftar",
+        url: "/",
+      },
+      {
+        id: 2,
+        label: "Pusat Bantuan",
+        url: "/",
+      },
+      {
+        id: 3,
+        label: "Tentang",
+        url: "/",
+      },
+      {
+        id: 4,
+        label: "Pers",
+        url: "/",
+      },
+      {
+        id: 5,
+        label: "Blog",
+        url: "/",
+      },
+      {
+        id: 6,
+        label: "Karir",
+        url: "/",
+      },
+      {
+        id: 7,
+        label: "Pengembang",
+        url: "/",
+      },
+    ],
+  },
+  {
+    id: 2,
+    heading: "Telusuri LinkedIn",
+    subMenu: [
+      {
+        id: 1,
+        label: "Learning",
+        url: "/",
+      },
+      {
+        id: 2,
+        label: "Pekerjaan",
+        url: "/",
+      },
+      {
+        id: 3,
+        label: "Gaji",
+        url: "/",
+      },
+      {
+        id: 4,
+        label: "Ponsel",
+        url: "/",
+      },
+      {
+        id: 5,
+        label: "Layanan",
+        url: "/",
+      },
+      {
+        id: 6,
+        label: "Produk",
+        url: "/",
+      },
+    ],
+  },
+  {
+    id: 3,
+    heading: "Produk Bisnis",
+    subMenu: [
+      {
+        id: 1,
+        label: "Bakat",
+        url: "/",
+      },
+      {
+        id: 2,
+        label: "Pemasaran",
+        url: "/",
+      },
+      {
+        id: 3,
+        label: "Penjualan",
+        url: "/",
+      },
+      {
+        id: 4,
+        label: "Lerning",
+        url: "/",
+      },
+    ],
+  },
+  {
+    id: 4,
+    heading: "Direktori",
+    subMenu: [
+      {
+        id: 1,
+        label: "Anggota",
+        url: "/",
+      },
+      {
+        id: 2,
+        label: "Pekerjaan",
+        url: "/",
+      },
+      {
+        id: 3,
+        label: "Perusahaan",
+        url: "/",
+      },
+      {
+        id: 4,
+        label: "Difiturkan",
+        url: "/",
+      },
+      {
+        id: 5,
+        label: "Learning",
+        url: "/",
+      },
+      {
+        id: 6,
+        label: "Posting",
+        url: "/",
+      },
+      {
+        id: 7,
+        label: "Artikel",
+        url: "/",
+      },
+      {
+        id: 7,
+        label: "Sekolah",
+        url: "/",
+      },
+      {
+        id: 8,
+        label: "Berita",
+        url: "/",
+      },
+      {
+        id: 9,
+        label: "Buletin",
+        url: "/",
+      },
+      {
+        id: 10,
+        label: "Layanan",
+        url: "/",
+      },
+      {
+        id: 11,
+        label: "Produk",
+        url: "/",
+      },
+      {
+        id: 12,
+        label: "Saran",
+        url: "/",
+      },
+      {
+        id: 13,
+        label: "Pencarian Orang",
+        url: "/",
+      },
+    ],
+  },
+];
+
+// footerMenuBottom
+export const footerMenuBottom = [
+  {
+    id: 1,
+    label: "Tentang",
+    url: "/",
+  },
+  {
+    id: 2,
+    label: "Aksesibilitas",
+    url: "/",
+  },
+  {
+    id: 3,
+    label: "Perjanjian Pengguna",
+    url: "/",
+  },
+  {
+    id: 4,
+    label: "Kebijakan Privasi",
+    url: "/",
+  },
+  {
+    id: 5,
+    label: "Kebijakan Cookie",
+    url: "/",
+  },
+  {
+    id: 6,
+    label: "Kebijakan Hak Cipta",
+    url: "/",
+  },
+  {
+    id: 7,
+    label: "Kebijakan Merek",
+    url: "/",
+  },
+  {
+    id: 8,
+    label: "Pengaturan Tamu",
+    url: "/",
+  },
+  {
+    id: 9,
+    label: "Panduan Komunitas",
+    url: "/",
+  },
+];
